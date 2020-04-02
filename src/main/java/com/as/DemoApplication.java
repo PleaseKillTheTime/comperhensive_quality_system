0d4f48ebbb8338490535b399636d6978fac09ed4
Shirk Lan <1445473963@qq.com>
2020-04-02 17:23:50 +0800

    配置

--- src/main/java/com/as/DemoApplication.java
+++ src/main/java/com/as/DemoApplication.java
@@ -1,7 +1,11 @@
+package com.as;
+
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.as"})
+@MapperScan("com.as.dao")
 public class DemoApplication {
 
     public static void main(String[] args) {
